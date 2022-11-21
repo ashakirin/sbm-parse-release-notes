f79e7f1c30642ccb9f9add7c40ffd0ad7597dc95
Andrei Shakirin <andrei.shakirin@gmail.com>
2022-11-21 13:14:53 +0100

    Added command line arguments

--- src/main/java/org/springframework/sbm/spike/releasenotes/ReleaseNotesMain.java
+++ src/main/java/org/springframework/sbm/spike/releasenotes/ReleaseNotesMain.java
@@ -9,15 +9,21 @@ import java.util.*;
 public class ReleaseNotesMain {
 
     public static void main(String[] args) throws IOException {
-        String targetDirectory = "/Users/ashakirin/DevResources/SpringBootMigrator/out";
+        if (args.length != 4) {
+            System.out.println("Please use following args: OUT_DIRECTORY GITHUB_USER GITHUB_TOKEN GITHUB_REPO");
+        }
+        String targetDirectory = args[0];
+        String user = args[1];
+        String token = args[2];
+        String repo = args[3];
         String currentPath = new java.io.File(".").getCanonicalPath();
         List<ReleaseItem> releaseItems = new ReleaseNotesParser().parse(currentPath + "/release-notes");
         new YamlBuilder().buildYamls(targetDirectory, releaseItems);
 
-        GitHubController gitHubController = new GitHubController("ashakirin", "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", "sbm-parse-release-notes");
+        GitHubController gitHubController = new GitHubController(user, token, repo);
         releaseItems.forEach(ri -> {
             try {
-                IssueIdentifier id = gitHubController.createIssue("ReleaseNotes: " + ri.getTitle(), ri.getDescription(), "ashakirin", List.of("SBM", "ReleaseNotes"));
+                IssueIdentifier id = gitHubController.createIssue("ReleaseNotes: " + ri.getTitle(), ri.getDescription(), user, List.of("SBM", "ReleaseNotes"));
                 new YamlBuilder().updateAndRenameWithIssueId(targetDirectory, ri, id);
                 System.out.println("Created: " + ri.getTitle() + "with id:" + id.getUrl());
             } catch (IOException e) {
